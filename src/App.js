1469ff3073d107dbc5b88605120017bb71271f93
mohammad <mohammadbabakhani2003@gmail.com>
2026-07-14 21:49:11 +0330

    my first project with html and css in 2021

--- src/App.js
+++ src/App.js
@@ -1,24 +1,334 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <body>
+        <header id="main-header" className="clearfix">
+          <div style={{ width: "70%", margin: "0 auto" }}>
+            <h1>Mohammad Babakhani page</h1>
+            <nav>
+              <ul>
+                <li id="list-drop">
+                  <a href="#">Drop Down</a>
+                  <div id="drop">
+                    <a style={{ display: "block", padding: "20px" }} href="#">
+                      Link1
+                    </a>
+                    <a style={{ display: "block", padding: "20px" }} href="#">
+                      Link2
+                    </a>
+                    <a style={{ display: "block", padding: "20px" }} href="#">
+                      Link3
+                    </a>
+                  </div>
+                </li>
+                <li>
+                  <a href="ListeKharid.html">Shopping list</a>
+                </li>
+                <li>
+                  <a href="Form.html">Sign Up</a>
+                </li>
+              </ul>
+            </nav>
+          </div>
+        </header>
+
+        <div id="flex">
+          <div id="secas">
+            <aside id="featured-article">
+              <figure>
+                <img
+                  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSVLZPzcZusUXmSMzZ1DTbkoOO30s74qxcueP9I_bE6rqWCsN8DQi6ViMK7VaxZypwMmns&usqp=CAU"
+                  alt="Baby Siavash"
+                />
+              </figure>
+              <header>
+                <h2>Featured Article</h2>
+                <h3>
+                  <a href="featured.html">Babi Murly Picture</a>
+                </h3>
+              </header>
+              <p>
+                Magna enim mollit adipisicing incididunt id dolore aliquip
+                adipisicing nulla cillum veniam reprehenderit non sint aute elit
+                quis sunt nisi consequat sunt nostrud sit. <br />
+                Magna enim mollit adipisicing incididunt id dolore aliquip
+                adipisicing nulla cillum veniam reprehenderit non sint aute elit
+                quis sunt nisi consequat sunt nostrud sit.
+              </p>
+            </aside>
+
+            <section id="footer-link">
+              <div className="column">
+                <h3>Browse</h3>
+                <ul>
+                  <li>
+                    <a href="#">
+                      Amet pariatur irure irure ea magna in dolore commodo
+                      deserunt qui nulla enim sit ut sit nisi enim
+                      reprehenderit.
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                      Culpa nostrud consectetur exercitation qui officia
+                      deserunt voluptate ut consequat non deserunt.
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                      Dolor ullamco occaecat aliqua est dolor eiusmod nostrud
+                      pariatur adipisicing sunt velit ad.
+                    </a>
+                  </li>
+                </ul>
+              </div>
+              <div className="column">
+                <h3>Social</h3>
+                <ul>
+                  <li>
+                    <a
+                      rel="external"
+                      className="social-link twitter"
+                      href="https://twitter.com/mohammad"
+                    >
+                      Twitter
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      rel="external"
+                      className="social-link instagram"
+                      href="https://instagram.com/mohammad"
+                    >
+                      Instagram
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      rel="external"
+                      className="social-link facebook"
+                      href="https://facebook.com/babakhani"
+                    >
+                      Facebook
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </section>
+          </div>
+
+          <main id="main">
+            <ol>
+              <li>
+                <article>
+                  <header>
+                    <footer>
+                      <time datetime="2020-06-12T23:00+3:30">
+                        June 12, 2020 - 11:00pm
+                      </time>
+                      <address>
+                        By <a href="mohammad.html">Mohammad Babakhani</a>
+                      </address>
+                    </footer>
+                    <h2>
+                      <a href="post1.html">
+                        Amet sunt ex do deserunt duis et nostrud duis consequat
+                        ut in ut excepteur tempor sit cillum ullamco ullamco.
+                      </a>
+                    </h2>
+                  </header>
+                  <div>
+                    <p>
+                      Dolore adipisicing aute ex eu qui consectetur sed aliquip
+                      officia magna deserunt labore magna quis nulla est officia
+                      pariatur nostrud et tempor proident eiusmod aliquip elit
+                      elit laborum ullamco dolore ut cillum labore ut mollit
+                      deserunt sunt velit do fugiat sint duis dolore irure amet
+                      consequat commodo sunt magna qui cillum aliqua est duis
+                      deserunt dolore occaecat in in sunt dolor aliquip in
+                      excepteur excepteur tempor dolor occaecat deserunt culpa
+                      eu proident dolore dolor ut irure commodo dolor duis ex
+                      esse magna occaecat reprehenderit fugiat dolore occaecat
+                      ut enim esse dolore cupidatat id nostrud incididunt
+                      exercitation deserunt dolor minim aliquip esse
+                      reprehenderit aliquip ut eiusmod occaecat pariatur sed
+                      enim in qui sint deserunt exercitation cupidatat ut nisi
+                      officia aute dolore consectetur id pariatur ad aute et
+                      magna occaecat est officia elit nulla ad ut incididunt
+                      nisi tempor in ad ad ex excepteur est incididunt sunt anim
+                      exercitation sint fugiat incididunt nisi non mollit in
+                      veniam dolore eiusmod ex nostrud sint laboris nostrud ea
+                      nostrud voluptate sit consequat pariatur elit fugiat id do
+                      aliqua cillum incididunt laboris voluptate duis ea dolor
+                      nisi eu in veniam adipisicing in sunt excepteur qui esse
+                      minim duis exercitation sunt mollit officia exercitation
+                      velit duis quis non duis ea dolore sint reprehenderit elit
+                      dolor mollit amet veniam incididunt consectetur et amet
+                      nulla consectetur dolore reprehenderit et labore qui
+                      deserunt sint anim consectetur mollit elit excepteur nulla
+                      sint cillum.
+                    </p>
+                  </div>
+                </article>
+              </li>
+
+              <li>
+                <article>
+                  <header>
+                    <footer>
+                      <time datetime="2020-06-12T23:00+3:30">
+                        June 12, 2020 - 11:00pm
+                      </time>
+                      <address>
+                        By <a href="ali.html">Mohammad Babakhani</a>
+                      </address>
+                    </footer>
+                    <h2>
+                      <a href="post2.html">
+                        Duis sed eiusmod aliqua excepteur sit consectetur dolor
+                        laboris nisi.
+                      </a>
+                    </h2>
+                  </header>
+                  <div>
+                    <p>
+                      Lorem ipsum pariatur excepteur veniam ut nulla consectetur
+                      exercitation aliquip cillum officia magna ex in eiusmod
+                      mollit sunt nostrud commodo tempor quis duis ut irure
+                      velit pariatur dolor adipisicing in officia aliquip
+                      laboris proident non consectetur ullamco nostrud sint est
+                      deserunt sint ut anim labore dolore ex aliqua in qui ut
+                      velit mollit excepteur tempor in amet reprehenderit dolore
+                      id esse occaecat ut deserunt eiusmod minim ut sed ut anim
+                      in eu dolor minim ea consectetur ex laboris labore dolore
+                      incididunt sed anim eiusmod ut aliqua sint aliquip
+                      incididunt ut amet duis esse ullamco tempor ad est dolor
+                      duis velit esse do commodo aute ut do velit pariatur sed
+                      laboris proident ad elit consequat nisi ex non tempor
+                      magna fugiat sint ullamco nostrud qui duis consequat
+                      aliquip in cillum voluptate enim minim dolore magna
+                      commodo ea nisi est anim labore sit sunt esse
+                      reprehenderit irure laboris velit ea laboris sunt cillum
+                      adipisicing id veniam aliquip in dolor voluptate
+                      reprehenderit nisi dolore adipisicing et reprehenderit
+                      nulla occaecat exercitation amet enim nulla veniam
+                      excepteur occaecat id commodo minim in cillum aliqua amet
+                      sint adipisicing in non ad adipisicing magna occaecat
+                      laborum qui pariatur occaecat irure cillum magna sunt do
+                      est magna consectetur ad ullamco anim ullamco ea ut ex ad
+                      magna magna aute ea irure commodo ut officia ut sint ut
+                      non labore culpa dolor veniam ut.
+                    </p>
+                    <figure>
+                      <img
+                        width="320"
+                        src="https://roozaneh.net/wp-content/uploads/2021/06/cr7-4.jpg"
+                        alt="Baby Ronaldo"
+                      />
+                      <figcaption>A picture of Ronaldo</figcaption>
+                    </figure>
+                    <p>
+                      Veniam fugiat exercitation mollit reprehenderit proident
+                      deserunt velit proident laboris in incididunt anim
+                      proident ea do esse dolor ut elit laboris consequat minim
+                      in qui amet do nostrud sint fugiat consequat in culpa. Ut
+                      ut minim consequat mollit in mollit excepteur consectetur
+                      sed mollit anim non est aliqua dolor in sunt dolore et non
+                      laboris sed incididunt occaecat enim exercitation in do
+                      irure eiusmod in sed voluptate aute duis ullamco fugiat
+                      amet.
+                    </p>
+                  </div>
+                  <details>
+                    <summary>Click here to see more</summary>
+                    <p>
+                      Minim nostrud nulla excepteur adipisicing consectetur esse
+                      exercitation exercitation dolore dolore id nulla et qui
+                      amet nulla sit nulla quis laborum quis. Elit et excepteur
+                      consequat elit in dolore dolore laborum sit reprehenderit
+                      qui aliquip dolor aliqua culpa dolore officia veniam
+                      deserunt.
+                    </p>
+                  </details>
+                </article>
+              </li>
+
+              <li>
+                <article>
+                  <header>
+                    <footer>
+                      <time datetime="2020-06-12T23:00+3:30">
+                        June 12, 2020 - 11:00pm
+                      </time>
+                      <address>
+                        By <a href="mamad.html">Mohammad Babakhani</a>
+                      </address>
+                    </footer>
+                    <h2>
+                      <a href="post3.html">
+                        Officia sint ea incididunt ullamco voluptate excepteur
+                        incididunt velit anim aliqua.
+                      </a>
+                    </h2>
+                  </header>
+                  <div>
+                    <p>
+                      In consequat tempor nostrud laboris eiusmod eu adipisicing
+                      dolore minim sit incididunt qui cillum exercitation
+                      aliquip duis consequat quis id cupidatat enim sed ut dolor
+                      laboris non nisi dolor duis ut incididunt nulla cillum in
+                      nostrud adipisicing sit irure et in nisi veniam quis
+                      incididunt cupidatat ea veniam mollit reprehenderit veniam
+                      sint exercitation qui sed eiusmod ut in aliqua est aute
+                      exercitation culpa sunt reprehenderit sed id in ad
+                      reprehenderit sint exercitation elit irure esse id veniam
+                      officia enim reprehenderit magna amet in veniam esse
+                      cupidatat sunt sunt magna id adipisicing aute commodo
+                      dolore deserunt quis voluptate tempor magna et mollit sunt
+                      ut esse proident eiusmod ut ut esse sit veniam deserunt
+                      pariatur cillum irure elit pariatur id qui exercitation
+                      minim ut nisi magna est qui eu incididunt qui ea cupidatat
+                      ullamco dolor dolor fugiat dolor nostrud dolor eiusmod
+                      culpa pariatur reprehenderit quis elit qui enim consequat
+                      do adipisicing enim reprehenderit id pariatur sit proident
+                      cupidatat pariatur aute pariatur enim tempor fugiat velit
+                      aliqua nisi nostrud non irure dolor mollit veniam ut
+                      dolore ea irure proident sed dolore amet eu exercitation
+                      qui dolore esse sed aliqua ullamco magna aliqua ut in
+                      fugiat tempor ut amet nulla dolore anim ad aliquip eiusmod
+                      est magna tempor aute commodo ea amet nisi dolore anim
+                      incididunt consequat aliqua aute sint ullamco elit
+                      proident aliqua laboris est sit esse eu elit deserunt est
+                      in proident adipisicing sit mollit et cillum id minim elit
+                      sit nulla laboris eiusmod sunt anim exercitation elit et
+                      enim amet cupidatat occaecat aliquip dolor in qui nisi
+                      eiusmod ex.
+                    </p>
+                  </div>
+                </article>
+              </li>
+            </ol>
+          </main>
+        </div>
+
+        <footer className="clearfix">
+          <div className="footer">
+            <span>&copy; Mohammad Babakhani 2020</span>
+            <ul>
+              <li>
+                <a href="privacy.html">Privacy Policy</a>
+              </li>
+              <li>
+                <a href="terms.html">Terms of Service</a>
+              </li>
+              <li>
+                <a href="mailto:mamadi@gmail.com">Support</a>
+              </li>
+            </ul>
+          </div>
+        </footer>
+      </body>
+    </>
   );
 }
 
